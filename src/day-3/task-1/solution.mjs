309f31ab72967ec8bbb9fba36d155ba57e8e4c49
Serj Komarovsky <serj.komarovsky@gmail.com>
2022-12-03 20:40:27 +0400

    fix

--- src/day-3/task-1/solution.mjs
+++ src/day-3/task-1/solution.mjs
@@ -10,34 +10,19 @@ const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'
 const getPriority = rucksackItems => {
     const genericItemType = getGenericItemType(rucksackItems);
     const shift = genericItemType === genericItemType.toUpperCase() ? 27 : 1;
-    // console.log('priority: ', genericItemType, shift + letters.indexOf(genericItemType.toLowerCase()))
+
     return shift + letters.indexOf(genericItemType.toLowerCase());
 };
 
-const getGenericItemType = ([firstRucksackItem = [], secondRucksackItem = []]) => {
-    let generic = null;
-    for(let i = 0; i < firstRucksackItem.length; i++) {
-        const currentItem = firstRucksackItem[i];
-        const hasMatch = secondRucksackItem.includes(currentItem);
-        if(hasMatch) {
-            generic = currentItem;
-            break;
-        }
-    }
-
-    return generic;
-}
+const getGenericItemType = ([firstRucksackItem = [], secondRucksackItem = []]) => firstRucksackItem.find(item => secondRucksackItem.find(secondItem => item === secondItem));
 
 const main = () => {
     const rucksackList = readSource('./input.txt');
 
-    return rucksackList.reduce((prioritySum, content) => {
-        if(!content) {
-            return prioritySum;
-        }
-
-        return prioritySum+= getPriority(getRucksackItems(content));
-    }, 0)
+    return rucksackList.reduce((prioritySum, content) =>
+        prioritySum + getPriority(getRucksackItems(content)),
+        0
+    );
 
 }
 
--- src/readSource.mjs
+++ src/readSource.mjs
@@ -1,3 +1,3 @@
 import fs from 'fs';
 
-export const readSource = (path) => fs.readFileSync(path, 'utf-8').split('\n');
+export const readSource = (path) => fs.readFileSync(path, 'utf-8').trim().split('\n');
